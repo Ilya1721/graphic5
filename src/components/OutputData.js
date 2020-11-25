4909fbe27f264964c79a39c27b581dc1a6b12609
Ilya1721 <ilyavusnya@gmail.com>
2020-11-25 14:48:13 +0200

    commit

--- src/components/OutputData.js
+++ src/components/OutputData.js
@@ -111,35 +111,31 @@ class OutputData extends React.Component {
     const factorial = this.factorial;
 
     let ps1 = 0;
+    const t = 1 / fl;
 
-    for (let i = omega2; i <= omega2 + s; i++) {
-      const n = omega2 + s;
-      const k = i;
-      let c = factorial(n) / (factorial(n - k) * factorial(k));
-      //console.log(c);
-      c *= pow(p, k) * pow(1 - p, n - k);
-      //console.log(pow(p, k));
-      //console.log(c);
+    for (let i = 0; i <= omega2 - s; i++) {
+      const n = omega2;
+      let c = factorial(n) / (factorial(n - i) * factorial(i));
+      c *= pow(E, -fl * (omega2 - i) * t) * pow(1 - pow(E, -fl * t), i);
       ps1 += c;
     }
 
     let temp = 0;
-    for (let i = omega2; i <= omega2 + s; i++) {
-      temp += 1 / i;
+    for (let i = 0; i <= omega2 - s; i++) {
+      temp += 1 / (s + i);
     }
-    const tt1 = t2 * temp;
+    const tt1 = (1 / fl) * temp;
 
-    const t = 1 / fl;
-    let temp2 = 1 + omega1 * fl * t;
-    for (let i = 2; i < s; i++) {
-      temp2 += pow(omega1 * fl * t, i) / factorial(i);
+    let temp2 = 0;
+    for (let i = 0; i < s; i++) {
+      temp2 += pow(fl * t, i) / factorial(i);
     }
     //console.log(temp2);
     //console.log(-omega2 * fl * t);
     //console.log(pow(E, -omega2 * fl * t));
 
-    const ps2 = pow(E, -omega1 * fl * t) * temp2;
-    const tt2 = t1 * ((s + 1) / omega1);
+    const ps2 = pow(E, -fl * t) * temp2;
+    const tt2 = ((s + 1) / omega1) * t1;
 
     console.log(ps1);
     console.log(ps2);
@@ -147,11 +143,17 @@ class OutputData extends React.Component {
     //console.log(tt2);
 
     return {
-      p: 0,
+      ps1,
+      tt1,
+      ps2,
+      tt2,
     };
   };
 
   factorial = (n) => {
+    if (n === 0 || n === 1) {
+      return 1;
+    }
     let f = 1;
     for (let i = 1; i <= n; i++) {
       f *= i;
@@ -161,11 +163,123 @@ class OutputData extends React.Component {
   };
 
   render() {
-    const answer = this.calculate();
+    const {
+      firstTask,
+      secondTask,
+      thirdTask,
+      fourthTask,
+      fifthTask,
+    } = this.calculate();
 
     return (
       <div className="output-data">
         <h3>Завдання 1</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Загальне резервування</th>
+              <th>Роздільне резервування</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{firstTask.p1.toFixed(4)}</td>
+              <td>{firstTask.p2.toFixed(4)}</td>
+            </tr>
+          </tbody>
+        </table>
+        <h3 className="margin-top">Завдання 2</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Загальний випадок системи з навантаженим резервом</th>
+              <th>
+                Система з навантаженим резервом при експоненціальному розподілі
+                напрацювання елементу до відмови
+              </th>
+              <th>Для системи, яка складається з n ідентичних елементів</th>
+            </tr>
+            <tr>
+              <th>імовірність безвідмовної роботи системи </th>
+              <th>імовірність безвідмовної роботи системи </th>
+              <th>середнє напрацювання до відмови</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{secondTask.ps1.toFixed(4)}</td>
+              <td>{secondTask.ps2.toFixed(4)}</td>
+              <td>{secondTask.ts.toFixed(4)}</td>
+            </tr>
+          </tbody>
+        </table>
+        <h3 className="margin-top">Завдання 3</h3>
+        <table>
+          <thead>
+            <tr>
+              <th colSpan={2}>
+                Система з ненавантаженим резервом з ідеальним перемикачем
+              </th>
+              <th colSpan={2}>
+                Система з ненавантаженим резервом з неідеальним перемикачем
+              </th>
+            </tr>
+            <tr>
+              <th>двоелементна система</th>
+              <th>триелементна система</th>
+              <th>двоелементна система</th>
+              <th>триелементна система</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{thirdTask.p2s1.toFixed(4)}</td>
+              <td>{thirdTask.p3s1.toFixed(4)}</td>
+              <td>{thirdTask.p2s2.toFixed(4)}</td>
+              <td>{thirdTask.p3s2.toFixed(4)}</td>
+            </tr>
+          </tbody>
+        </table>
+        <h3 className="margin-top">Завдання 4</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Імовірність безвідмовної роботи системи</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{fourthTask.ps.toFixed(4)}</td>
+            </tr>
+          </tbody>
+        </table>
+        <h3 className="margin-top">Завдання 5</h3>
+        <table>
+          <thead>
+            <tr>
+              <th colSpan={2}>
+                Система з дробовим резервуванням та ненавантаженим резервом
+              </th>
+              <th colSpan={2}>
+                Система з дробовим резервуванням та навантаженим резервом
+              </th>
+            </tr>
+            <tr>
+              <th>імовірність безвідмовної роботи</th>
+              <th>середнє напрацювання до відмови</th>
+              <th>імовірність безвідмовної роботи</th>
+              <th>середнє напрацювання до відмови</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{fifthTask.ps1.toFixed(7)}</td>
+              <td>{fifthTask.tt1.toFixed(4)}</td>
+              <td>{fifthTask.ps2.toFixed(7)}</td>
+              <td>{fifthTask.tt2.toFixed(4)}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
